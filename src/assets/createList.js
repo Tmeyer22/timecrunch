961c9dfe806dd1eeb95d0a50bcf486055aa15153
Toby Meyer <tobymeyer512@gmail.com>
2024-03-06 13:39:48 -0700

    Including createList

---
+++ src/assets/createList.js
@@ -0,0 +1,29 @@
+import { listData } from "./listData"
+
+function shuffle(array){
+    for(let i = array.length-1; i > 0; i--){
+        const j = Math.floor(Math.random() * ( i +1));
+        [array[i], array[j]] = [array[j], array[i]]; 
+    }
+    return array;
+}
+
+function compareIndex(a,b){
+    return a.index - b.index;
+}
+
+export function createList() {
+    let listRandom = shuffle(listData);
+    let listShort = listRandom.splice(0,8)
+    let listShortSort = listShort.sort(compareIndex);
+    let count = 0
+    let listShortOrder = []
+    listShortSort.forEach((o) => {
+        let newO = { ...o, position: count }
+        count = count + 1
+        listShortOrder.push(newO)
+    }
+    )
+    let listShortRandom = listShortOrder.sort(() => Math.random() - 0.5)
+    return(listShortRandom)
+}
